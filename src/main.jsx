f6d4e019a753d9e1f763030d2ac25efff9b4b174
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-09-20 03:04:27 +0600

    react router installed and one home component with other two children created

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,23 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+import Footer from './components/Footer/Footer.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hi, This is home page.</div>
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/about",
+        element: <About></About>,
+      }, 
+      {
+        path: "/footer",
+        element: <Footer></Footer>
+      }
+    ]
   }
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
