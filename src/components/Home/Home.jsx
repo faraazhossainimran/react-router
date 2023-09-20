46b3f81e96c52538092c257ed1ce77b58ffac8f4
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-09-20 16:40:05 +0600

    react router location added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
-import { Outlet, useNavigation } from "react-router-dom"
+import { Outlet, useLocation, useNavigation } from "react-router-dom"
 import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
 import User from "../User/User"
 
 const Home = () => {
     const navigation = useNavigation()
+    const location = useLocation()
+    console.log(location);
     return (
         <div>
             <h1 className="text-3xl font-bold text-center py-8">Welcome to my website, this is home</h1>
