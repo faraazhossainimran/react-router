6edfc2003e48fa2559f3b98b3206f26b3a749606
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-09-20 15:01:31 +0600

    react router errorPage added

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -9,10 +9,10 @@ const handleDetails = () => {
 }
     return (
         <div className="container mx-auto">
-            <h1 className="text-xl"> {title}</h1>
+            <h1 className="text-3xl"> {title}</h1>
             {/* <Link></Link> */}
-            <Link to={`/post/${id}`}>show details</Link>
-            <button onClick={handleDetails}>Click to show details</button>
+            <Link to={`/post/${id}`}><button className="btn mx-4 my-4">Show details</button></Link>
+            <button className="btn" onClick={handleDetails}>Click to show details</button>
         </div>
     )
 }
